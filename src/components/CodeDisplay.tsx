284063f4e983c3786e4a316095c83c015376a4d9
HakimYu <yhjredin@gmail.com>
2025-09-26 14:07:42 +0800

    feat: 添加代码高亮

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -1,3 +1,5 @@
+import { Highlight, themes } from "prism-react-renderer";
+
 interface CodeDisplayProps {
   cssCode: string;
   htmlCode: string;
@@ -6,13 +8,47 @@ interface CodeDisplayProps {
 export default function CodeDisplay({ cssCode, htmlCode }: CodeDisplayProps) {
   return (
     <div className="code-display">
-      <h3>CSS :</h3>
-      <pre className="css-code">
-        {cssCode || "/* 请选择样式选项查看对应的CSS代码 */"}
-      </pre>
+      <div className="code-section">
+        <h3>CSS :</h3>
+        <Highlight
+          theme={themes.vsDark}
+          code={cssCode || "/* 请选择样式选项查看对应的CSS代码 */"}
+          language="css"
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      </div>
 
-      <h3>HTML :</h3>
-      <pre className="html-code">{htmlCode}</pre>
+      <div className="code-section">
+        <h3>HTML :</h3>
+        <Highlight
+          theme={themes.vsDark}
+          code={htmlCode}
+          language="html"
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      </div>
     </div>
   );
 }
